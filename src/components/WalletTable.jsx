4de4aa8c055148db7fd6c9a56536718f784126c5
Thiago Oliveira <thiago_marcos_@hotmail.com>
2022-07-28 22:46:28 -0300

    requisito 7 - completado

--- src/components/WalletTable.jsx
+++ src/components/WalletTable.jsx
@@ -1,7 +1,10 @@
+import PropTypes from 'prop-types';
 import React from 'react';
+import { connect } from 'react-redux';
 
 class WalletTable extends React.Component {
   render() {
+    const { expenses } = this.props;
     return (
       <div>
         <table>
@@ -18,10 +21,37 @@ class WalletTable extends React.Component {
               <th>Editar/Excluir</th>
             </tr>
           </thead>
+          <tbody>
+            {
+              expenses.map((expense) => {
+                const { value } = expense;
+                const { name, ask } = expense.exchangeRates[expense.currency];
+
+                return (
+                  <tr key={ expense.id }>
+                    <td>{expense.description}</td>
+                    <td>{expense.tag}</td>
+                    <td>{expense.method}</td>
+                    <td>{(+value).toFixed(2)}</td>
+                    <td>{name}</td>
+                    <td>{(+ask).toFixed(2)}</td>
+                    <td>{(+expense.value * ask).toFixed(2)}</td>
+                    <td>Real</td>
+                  </tr>
+                );
+              })
+            }
+          </tbody>
         </table>
       </div>
     );
   }
 }
 
-export default WalletTable;
+WalletTable.propTypes = {
+  expenses: PropTypes.arrayOf(PropTypes.any).isRequired,
+};
+
+const mapStateToProps = ({ wallet }) => wallet;
+
+export default connect(mapStateToProps)(WalletTable);
