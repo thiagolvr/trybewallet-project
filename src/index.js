00b83fe27a1831b3d611eafb856003e2edbe6854
Thiago Oliveira <thiago_marcos_@hotmail.com>
2022-09-05 12:35:11 -0300

    projeto atualizado para react 18

--- src/index.js
+++ src/index.js
@@ -1,15 +1,34 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
+import { ColorModeScript } from '@chakra-ui/react';
+import React, { StrictMode } from 'react';
+import * as ReactDOM from 'react-dom/client';
+import reportWebVitals from './reportWebVitals';
+import * as serviceWorker from './serviceWorker';
 import { BrowserRouter } from 'react-router-dom';
 import { Provider } from 'react-redux';
 import Routes from './routes/Routes';
 import store from './redux/store';
 
-ReactDOM.render(
-  <BrowserRouter>
-    <Provider store={ store }>
-      <Routes />
-    </Provider>
-  </BrowserRouter>,
-  document.getElementById('root'),
+const container = document.getElementById('root');
+const root = ReactDOM.createRoot(container);
+
+root.render(
+  <StrictMode>
+    <BrowserRouter>
+      <Provider store={store}>
+        <ColorModeScript />
+        <Routes />
+      </Provider>
+    </BrowserRouter>
+    ,
+  </StrictMode>
 );
+
+// If you want your app to work offline and load faster, you can change
+// unregister() to register() below. Note this comes with some pitfalls.
+// Learn more about service workers: https://cra.link/PWA
+serviceWorker.unregister();
+
+// If you want to start measuring performance in your app, pass a function
+// to log results (for example: reportWebVitals(console.log))
+// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+reportWebVitals();
